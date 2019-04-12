06e495c89cdbf2dbad36a5520019cd18aab07a20
harshita-d <hrs.dadhich@gmail.com>
2019-04-12 23:59:27 +0530

    main class for packages

---
+++ demo1.java
@@ -0,0 +1,10 @@
+import p1.p2.*;
+
+class demo1{
+public static void main(String[] args) {
+	protection obj1=new protection();
+	derived obj2=new derived();
+	samepackage obj3=new samepackage();
+	
+}
+}
---
+++ demo2.java
@@ -0,0 +1,8 @@
+package p2;
+import p2.*;
+class demo2{
+	public static void main(String[] args) {
+		protection2 obj1=new protection2();
+		otherpackage obj2=new otherpackage();
+	}
+}
